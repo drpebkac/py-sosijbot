a7fd88f58df4d6b45dc37d477d6cf1ebbcd1d0f0
Bob Nguyen <bob.nguyen@oobe.com.au>
2022-11-03 16:11:53 +1100

    idk anymore

--- main-ver0.5.py
+++ main-ver0.5.py
@@ -4,9 +4,7 @@ import sys
 import time
 import re
 
-#Global variables
-scopedairports = ["YSSY", "YBBN"]
-exemptstations = ["SY_TWR", "SY_GND", "BN_TWR", "BN_GND"]
+
 
 class Post:
   def create_Body_Departures():
@@ -31,16 +29,22 @@ class StartWorkFlow:
     print("")
 
 
+#Global variables
+scopedairports = ["YSSY", "YBBN"]
+exemptlist = ["SY_TWR", "SY_GND", "BN_TWR", "BN_GND"]
+function_lock = 0
 
+try:
+  exempt_sid = str(sys.argv[1])
+except:
+  print("No exception defined for controller_cid. Assigning except_controller_cid id to default")
+  exempt_sid = '0000000'
+
+#Add pilot/controller CID to exemptlist for later use
+exemptlist.append(exempt_sid)
 
 #Entry point
 while True:
-  try:
-    exempt_sid = str(sys.argv[1])
-  except:
-    print("No exception defined for controller_cid. Assigning except_controller_cid id to default")
-    exempt_sid = '0000000'
-
   #If vatsim api temporary goes offline, the program breaks, include exception handling to avoid the bot from crashing
   while True:
     try:
@@ -58,8 +62,10 @@ while True:
 
   #Conditions to run alerts
   #If exempt CID is connected as pilot
+  #Note this has nothing to do with the function mechanism for stopping the bot from spamming
+  #This is only to stop notifying if the exempt CID is connected to VATSIM, or if one of the scopedairports are online
   for i in pilot_data:
-    if str(i['cid']) == exempt_sid:
+    if str(i['cid']) == exemptlist:
       exempt_pilot_status = "online"
       break
     else:
@@ -67,20 +73,22 @@ while True:
 
   #If any of the scoped stations are online
   for a in controller_data :
-    if a['callsign'] in exemptstations:
+    if a['callsign'] in exemptlist:
       exempt_station_status = "online"
       break
     else:
       exempt_station_status = "offline"
 
   # Begin composing notification when either scoped controllers and pilot CID is offline 
+  # If both exempt status and function lock is 0, run if block and send notification
+  # function_lock stops the bot from spamming every 15 seconds
   if exempt_station_status == "offline" and exempt_pilot_status == "offline":
     print("Calling notification mechanism")
     print(exempt_pilot_status)
     print(exempt_station_status)
-  else: 
-    print("Scoped CIDs and stations are online, no notifications will be sent")
-
+  # If both exempt status is offline and function lock is 1, run if block and send notification
+  elif exempt_station_status == "online" or exempt_pilot_status == "online":
+    print("Hold off calling the notification classes")
     
   time.sleep(15.0)
 
